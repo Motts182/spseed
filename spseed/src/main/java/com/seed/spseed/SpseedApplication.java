8eff7a5f319e77f49042b5400f1aeadbbf944463
Motts <mnd.matias.diaz@gmail.com>
2019-10-12 09:55:52 -0300

    fist commit

---
+++ spseed/src/main/java/com/seed/spseed/SpseedApplication.java
@@ -0,0 +1,13 @@
+package com.seed.spseed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpseedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpseedApplication.class, args);
+	}
+
+}
---
+++ spseed/src/test/java/com/seed/spseed/SpseedApplicationTests.java
@@ -0,0 +1,16 @@
+package com.seed.spseed;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpseedApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
